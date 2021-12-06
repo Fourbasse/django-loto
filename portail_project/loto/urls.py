f0afd784431fa9a622091a7dde119e83b3bbc2dd
Fourbasse <benslidoux76@gmail.com>
2021-12-06 19:57:04 +0100

    Modif HTML;VIEWS;JS

--- portail_project/loto/urls.py
+++ portail_project/loto/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('index/',views.home, name="index"),
-    path('index/',views.EuromillionBdd, name="index"),
-    path('index/simulation',views.Simulation,name="simulation"),
+    path('index/loto_update',views.LotoUpdate, name="index"),
+    path('index/euromillion_update',views.EuromillionBdd, name="index"),
+    path('index/loto_simulation',views.Simulation,name="simulation"),
     path('index/loto_datatable',views.LotoDatatable,name="loto_data")
 ]
